4d212fe95830ab52066034e915b1ff566e78ae30
Awwab Ahmed <awwab.k.ahmed@gmail.com>
2024-06-02 16:24:32 -0400

    awwab's full code

--- Project 1-S24/linked_calc.cpp
+++ Project 1-S24/linked_calc.cpp
@@ -7,7 +7,12 @@ LinkedCalc<T>::LinkedCalc() : head(nullptr) {}
 // Destructor to deallocate memory
 template <typename T>
 LinkedCalc<T>::~LinkedCalc() {
-    //To be implemented
+    Node<T>* cur = head;
+    while(cur){
+        Node<T>* temp = cur;
+        cur = cur->next;
+        delete temp;
+    }
 }
 
 // Function to insert a new node at the end of the linked list
@@ -29,6 +34,27 @@ void LinkedCalc<T>::insert(const T& value) {
 template <typename T>
 bool LinkedCalc<T>::isDigit(const T& c) {
     return (c >= '0' && c <= '9') ;
+}
+
+//helper function to convert needed parts of expression to floats
+template <typename T>
+float LinkedCalc<T>::convertToFloat(Node<T>*& current){
+    float num = 0.0f;
+    
+    //iterates if value is digit or decimal 
+    while (current && (isDigit(current->data) || current->data == '.')) {
+        //if decimal it adds 1/10 of the value after the decimal to num
+        if (current->data == '.') {
+            current = current->next;
+            num += 0.1 * (current->data - '0');
+        //if before the decimal it adds the value to num
+        }else{
+            num += (current->data - '0');
+        }
+        current = current->next;
+    }
+    
+    return num;
 }
 
 // Checks if a char is an operator
@@ -69,6 +95,33 @@ bool LinkedCalc<T>::validateExpression() {
 // Function to evaluate the expression represented by the linked list
 template <typename T>
 float LinkedCalc<T>::evaluateExpression() {
-    //To be implemented
+    Node<T>* cur = head;
+    
+    //first float or int is calculated
+    float float1 = convertToFloat(cur);
+    
+    //iterating to find the operator
+    while(!(isOperator(cur->data))){
+        cur = cur->next;
+    }
+    char operation = cur->data;
+    
+    //second float or int is calculated
+    float float2 = convertToFloat(cur->next);
+
+    //return value is calculated based on the operator
+    float num = 0.0f;
+    if (operation == '+') {
+        num = float1 + float2;
+    }else if(operation == '-') {
+        num = float1 - float2;
+    } else if (operation == '*') {
+        num = float1 * float2;
+    } else if (operation == '/') {
+        num = float1 / float2;
+    }
+    return num;
 }
+
+
 
